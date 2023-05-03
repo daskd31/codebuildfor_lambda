ec5683f68d893dab7a748e8f67fed698ead51261
KanishkaDas <Kanishka.Das@wholefoods.com>
2023-05-03 20:28:59 +0530

    first commit

---
+++ lambda_function.py
@@ -0,0 +1,41 @@
+import json
+import boto3
+import os
+
+def lambda_handler(event, context):
+    
+    if event.get('detail',None) != None:
+        stateMachineArn = event.get('detail',{}).get('stateMachineArn',{})
+        stateMachineArn = stateMachineArn.rsplit(':')[-1]
+    else:
+        stateMachineArn = "No ARN provided"
+        
+    if event.get('detail',None) != None:
+        statusDetails = event.get('detail',{}).get('status',{})
+    else:
+        statusDetails = "No status provided "
+        
+    msgSubject = stateMachineArn + " - status " + statusDetails
+    
+    jsonFormat = json.dumps(event,indent = 6)
+    
+    client = boto3.client('sns')
+    
+    topicARN = os.environ['topicARN']
+    #print(topicARN)
+    
+    response = client.publish(
+        TargetArn = topicARN,
+        Message = jsonFormat,
+        Subject = msgSubject
+        )
+    
+    #print(jsonFormat)
+    
+    return {
+        'statusCode': 200,
+        'body': jsonFormat,
+         'headers': {
+            'Content-Type': 'text/json',
+        }
+    }
